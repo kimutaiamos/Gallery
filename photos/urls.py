c7a082a60bbb23c0e2793856ac02892edc9db918
KyleTrippK <kyle.kibet@student.moringaschool.com>
2021-11-27 13:49:14 +0300

    added view function to display navbar

--- photos/urls.py
+++ photos/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from django.urls.resolvers import urlPattern
 from .import views
 
-urlPattern=[
-    url('^',views.welcome,name= 'welcome'),
-]
+# urlPattern=[
+#     url('^',views.welcome,name= 'welcome'),
+# ]
