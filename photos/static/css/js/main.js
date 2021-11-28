b9c13a388f1ad66c72d649ad2f3b889cf67f417e
kimutaiamos <kyle.kibet@student.moringaschool.com>
2021-11-28 20:01:34 +0300

    added readme.md

--- photos/static/css/js/main.js
+++
@@ -1,9 +0,0 @@
-function copyImage(value) {
-    function handler(event){
-          event.clipboardData.setData('text/plain', value);
-          event.preventDefault();
-          document.removeEventListener('copy', handler, true);
-    }
-    document.addEventListener('copy', handler, true);
-    document.execCommand('copy');
-    }
